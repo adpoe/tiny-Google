684b7427b7b37f642acfea175f443397cff5cb28
Tony Poerio <tony@tonypoer.io>
2017-03-05 20:52:42 -0500

    finished preprocessing shell script. saving.

--- preprocess.sh
+++ preprocess.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
+
 # get the filename of all all files in current path
 dir=/Users/tony/Documents/_LEARNINGS/CLOUD/tiny-Google/books_preprocess
+
+# iterate through all files in directory
 for entry in "$dir"/*
 do
+  # print so users knows what files were updated
   echo "$entry"
+
+  # grab only the file name, and print it
   fname="${entry##*/}"
   echo "$fname"
-  # works but makes a file with -e appended in same directory
+
+  # using sed, add the file name a s prefix to EVERY line of the entry
   sed -i -e 's/^/'$fname'\\t/' $entry
-  #awk '$0="$fname"$0'
-  # add into a new folder with preprocessed data
-  #sed -e 's/^/prefix/' $entry > "$dir"/preprocessed/"$entry"
+
 done
+
+# remove intermediate files created by sed, now that we're done
+rm "$dir"/*.txt-e
+
+
+# And now we have preprocessed files. Each book has its filename and then a tab in each line.
+# This will make processing each book with an ID in MapReduce very simple.
+# --> As a bonus:  SED works extremely quickly. Should only take less than 1 second total
+#     to add this to all books.
--- preprocessing.py
+++
@@ -1,8 +0,0 @@
-""" File used to pre-process our data.
-        - Goal is to add the filename to start of each line, separated by a \t.
-        - This will make parsing input and adding document id's simpler
-"""
-
-import os
-import sys
-
