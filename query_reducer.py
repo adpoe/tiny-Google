4e48a6847ee13aeb08d0e537a4c5f94320d1a9c8
Andrew Masih <anm226@pitt.edu>
2017-03-24 20:10:25 -0400

    added a the concept of puting the context of a phrase, and some colors for aesthetics

--- query_reducer.py
+++ query_reducer.py
@@ -9,6 +9,16 @@ import os
 import ast
 import math
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 # both programs need to reference the same keys
 #keys = ['yours', 'you', 'yield', 'king', 'shield', 'young', 'yawned', 'yards', 'xfas']
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -113,54 +123,104 @@ print('=========================================================================
 # split the term, to go through
 search_list = search_term.split()
 
+save_line_num = {}
+#find if any words appear in the same context, in other words a phrase
+for x in range(0, 3):
+
+    #go through each word
+    for some_word in search_list:
+         #if there are an words at all
+         if sort_weights:
+             #find the word in index
+             if some_word in reducer_index:
+                 #get infor about the word
+                 for target_data in reducer_index[some_word]:
+
+                     #if it's the word from the book we are looking for
+                     if target_data[1] == sort_weights[x][0]:
+
+                         # get the first line number
+                         line_data = ast.literal_eval(target_data[2])
+
+                         #sort line data so, if it's a phrase the line numbers will be the same
+                         line_data.sort()
+
+                         #pick the first available line_num
+                         #AKA the first place it appears at
+                         line_num = line_data[0]  # could slice to :n, to get the first `n` numbers
+
+                         #record the doc we are in
+                         doc = target_data[1]
+
+                         #create a tuple with line number and doc
+                         target = (line_num, doc)
+
+                         #if it's already not in our dictionary, add it to the dictionarry
+                         #with prev tuple as the key
+                         if target not in save_line_num:
+                             #put the word at given tuple
+                             save_line_num[(line_num, doc)] = [some_word]
+                         else:
+                             #if it line_num and doc tuple is alread there, it means the word is
+                             #probably part of a phrase
+                             save_line_num[(line_num, doc)].append(some_word)
+         else:
+             print("No words found!")
+
+print_term = " "
 #print the top 3 results from the weights_per_doc
 for x in range(0, 3):
     print("\n")
     if sort_weights:
         #the total weight per doc, which is search_term[0]_weight + search_term[1]_weigth +..search_term[n]_weight
-        print("Result %d --> Weight = %f"%(x+1,sort_weights[x][1]))
+        #added some boldness to weight for aesthetics
+        print_weight = ("Weight = %f"%(sort_weights[x][1]))
+        print_weight = bcolors.BOLD + print_weight + bcolors.ENDC
+        print("Result %d --> %s"%(x+1,print_weight))
         #print title
-        print("TITLE : %s\n" %(sort_weights[x][0]))
+        cur_title = bcolors.OKBLUE+ bcolors.BOLD +  sort_weights[x][0] + bcolors.ENDC
+        print("TITLE : %s\n" %(cur_title))
 
-    #print context for each word
-    for some_word in search_list:
-        if some_word in reducer_index:
-            for target_data in reducer_index[some_word]:
-                if target_data[1] == sort_weights[x][0]:
+    for some_line in save_line_num:
+        if some_line[1]==sort_weights[x][0]:
 
+            #display what key words the context is for
+            word_list = save_line_num[some_line]
+            print_term = ", ".join(word_list)
+            print("Earliest context found for terms : %s"%(print_term))
+            print("---------------------------------------------------------------------------------------------------------")
+            print("---------------------------------------------------------------------------------------------------------")
 
-                    # get the first line number
-                    line_data = ast.literal_eval(target_data[2])
-                    line_num = line_data[0]  # could slice to :n, to get the first `n` numbers
-                    # get the filename
-                    fname = target_data[1]
+            #get line number
+            line_num = some_line[0]
 
-                    # get the directory
-                    dir_path = os.path.dirname(os.path.realpath(__file__))
+            # get the filename
+            fname = some_line[1]
 
-                    # concat full path for our file
-                    fpath = dir_path + '/books/' + fname
+            # get the directory
+            dir_path = os.path.dirname(os.path.realpath(__file__))
 
-                    # only the grab the context for lines right our around our target
-                    context = ""
-                    with open(fpath) as f:
-                        for i, line in enumerate(f):
-                            if i == line_num - 2:
-                                context += '\t' + line
-                            if i == line_num - 1:
-                                context += '\t' + line
-                            if i == line_num:
-                                context += '\t' + line
-                            if i == line_num + 1:
-                                context += '\t' + line
-                            if i == line_num + 2:
-                                context += '\t' + line
-                    #print the context around the words in the search term
-                    print(context)
-        else:
-            print('-------------------------------------------')
-            print('This term does not appear: "%s"' % some_word)
-            print('-------------------------------------------')
+            # concat full path for our file
+            fpath = dir_path + '/books/' + fname
+
+            # only the grab the context for lines right our around our target
+            context = ""
+            with open(fpath) as f:
+                for i, line in enumerate(f):
+                    if i == line_num - 2:
+                        context += '\t' + line
+                    if i == line_num - 1:
+                        context += '\t' + bcolors.OKGREEN + line + bcolors.ENDC #added color for aesthetics
+                    if i == line_num:
+                        context += '\t' + line
+                    if i == line_num + 1:
+                        context += '\t' + line
+                    if i == line_num + 2:
+                        context += '\t' + line
+            #print the context around the words in the search term
+            print(context)
+            print("---------------------------------------------------------------------------------------------------------")
+            print("---------------------------------------------------------------------------------------------------------")
 
 
 
@@ -213,7 +273,7 @@ for key in keys:
                         if i == line_num - 2:
                             context += '\t' + line
                         if i == line_num - 1:
-                            context += '\t' + line
+                            context += '\t' + bcolors.OKGREEN + line + bcolors.ENDC
                         if i == line_num:
                             context += '\t' + line
                         if i == line_num + 1:
@@ -222,8 +282,15 @@ for key in keys:
                             context += '\t' + line
 
                 # and print our result!
-                print('Result %d for search term ---> `%s`:\nTITLE: %s\t >> Word Occurrences: %d << \n%s' %
-                      ((idx + 1), key, fname, count, context))
+
+                #bold count for aesthetics
+                occur = bcolors.BOLD + str(count) + bcolors.ENDC
+
+                #color book title for aesthetics
+                fname = bcolors.OKBLUE + bcolors.BOLD + fname + bcolors.ENDC
+
+                print('Result %d for search term ---> `%s`:\nTITLE: %s\t >> Word Occurrences: %s << \n%s' %
+                      ((idx + 1), key, fname, occur, context))
 
 
     # if we don't have the word, display this result for user
